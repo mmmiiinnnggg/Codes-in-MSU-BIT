5d8f63973f65ce150982e718dc0823d0a2e85c0c
Xumingchuan <xumingchuan@yeah.net>
2021-03-15 15:14:17 +0800

    Create list.h

---
+++ Object Oriented Programming C++/List/list.h
@@ -0,0 +1,36 @@
+# include<iostream>
+using namespace std;
+class List {
+  struct Node {
+      char elem;
+      Node* next;
+      Node (char elem='_', Node* next=NULL) {
+			this->elem=elem;
+			this->next=next;
+      }
+  };
+  Node* lst;
+  void clear();
+public:
+
+  List ();
+
+  List (char c);
+
+  List (const char* s);
+
+
+  bool empty() const ;
+
+  List (const List& l);
+
+  ~List();
+
+
+   List operator+ (const List& l);
+
+   friend ostream& operator<< (ostream& s, const List& l);
+
+
+   List& operator= (const List& l);
+};
