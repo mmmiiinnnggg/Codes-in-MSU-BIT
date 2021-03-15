50f8f975db67f1bd9f27168bb42fbe090c83aeee
Xumingchuan <xumingchuan@yeah.net>
2021-03-15 15:15:02 +0800

    Create divide.c

---
+++ Practicum_3_OS/MyShell/divide.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+char cmd[30];
+char buf[10][10];
+int command[30]={0};//mark the command line in 2-dim buf
+
+//put command line to a 2-dim matrix
+int collect(){ 
+  int i=0,j=0,k=0,index=0;
+  char a,b;
+  fgets(cmd,30,stdin);
+  while((a=cmd[index])!='\n')
+    switch(a){
+      case '>': if((b=cmd[index+1])=='>') {i++;j=0;buf[i][j]=a;j++;buf[i][j]=b;command[i]=1;i++;j=0;index+=2;break;}//>>
+                else {i++;j=0;buf[i][j]=a;command[i]=2;i++;j=0;buf[i][j]=b;j++;index+=2;break;}//>
+      case '|': if((b=cmd[index+1])=='|') {i++;j=0;buf[i][j]=a;j++;buf[i][j]=b;command[i]=3;i++;j=0;index+=2;break;}//||
+                else {i++;j=0;buf[i][j]=a;command[i]=4;i++;j=0;buf[i][j]=b;j++;index+=2;break;}//|
+      case '<': {i++;j=0;buf[i][j]=a;command[i]=5;i++;j=0;index++;break;}
+      case '&': {i++;j=0;buf[i][j]=a;command[i]=6;i++;j=0;index++;break;}
+      case ';': {i++;j=0;buf[i][j]=a;command[i]=7;i++;j=0;index++;break;}
+      case '(': if(index==0) {buf[i][j]=a;command[i]=8;index++;break;}
+                else {i++;j=0;buf[i][j]=a;command[i]=8;index++;break;}
+      case ')': if(index==0) {buf[i][j]=a;command[i]=8;index++;break;}
+                else {i++;j=0;buf[i][j]=a;command[i]=9;index++;break;}
+      case ' ': if((b=cmd[index+1])=='&') {i++;j=0;buf[i][j]=a;command[i]=10;i++;j=0;buf[i][j]=b;command[i]=6;i++;j=0;index+=2;break;}//||
+                else {i++;j=0;buf[i][j]=a;command[i]=10;i++;j=0;buf[i][j]=b;j++;index+=2;break;}//|
+      default : if((cmd[index-1]!='(')&&(cmd[index-1]!=')')) {buf[i][j]=a;j++;index++;break;}
+                else {i++;j=0;buf[i][j]=a;j++;index++;break;}
+    }
+    return i;//i-lines
+}
+
+int main(){
+  collect();
+/*
+  for(int i=0;i<10;i++){
+    for(int j=0;j<10;j++)
+      printf("%c",buf[i][j]);
+    printf("\n");
+  }
+*/
+  /*for(int k=0;k<30;k++)  
+    printf("%c ",cmd[k]);*/
+  for(int i=0;i<10;i++){
+    printf("%s",buf[i]);
+    printf(" %ld",strlen(buf[i]));
+    printf("\n");
+  }
+  for(int j=0;j<30;j++)
+      printf("%d ",command[j]);
+  return 0;	    
+} 
