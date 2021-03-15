385751064ca335558e7d10b0f105a9df8506f17c
Xumingchuan <xumingchuan@yeah.net>
2021-03-15 15:17:51 +0800

    Create work4_5.c

---
+++ Practicum_3_OS/Seminar CODE/work4 direction/work4_5.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<stdlib.h>
+#include<fcntl.h>
+
+//cat<f1>f2
+int main(){
+  int fd1,fd2;
+  if(0>(fd1=open("f1.txt",O_WRONLY|O_CREAT,0644))) {perror("open");exit(1);}
+  if(0>(fd2=open("f2.txt",O_WRONLY|O_CREAT,0644))) {perror("open");exit(1);}
+  if(0>(fd=open("f.txt",O_WRONLY|O_CREAT,0644))) {perror("open");exit(1);}
+  dup2(fd,0)
+  dup2(fd2,1);
+  close(fd2);
+  execlp("cat","cat","f1.txt",NULL);
+  perror("exec");
+  return 0;
+}
